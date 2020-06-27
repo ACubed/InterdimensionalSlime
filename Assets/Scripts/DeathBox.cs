4ea52ddfb5ba85947f5b763034f1773e3a5da8c3
Arvind Maan <arvindmaan1199@gmail.com>
2020-06-27 16:23:37 -0500

    Added death

--- Assets/Scripts/DeathBox.cs
+++ Assets/Scripts/DeathBox.cs
@@ -1,23 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DeathBox : MonoBehaviour
 {
 
-    public Resetter hero;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.tag == "Player")
+        if (collision.collider.tag == "Player")
         {
-            hero.respawn();
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
         } //end of collision detection 
     }
 }
